786c735faa13b99df3bd943265bc97f9d6733e40
MeliLiz <melifernandez@ciencias.unam.mx>
2024-02-13 19:06:29 -0600

    Cambie Script 3

--- intro_python/Script3.py
+++ intro_python/Script3.py
@@ -1,15 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def funcion(x):
-    return x ** 2 + 3
 
-x = np.linspace(-5, 5, 10)
-y = funcion(x)
+x = np.arange(-5,5,0.1)
+y = x**3 + 5
 
 plt.plot(x,y)
-plt.xlabel("x")
-plt.ylabel("y")
-plt.title("x^2 + 3 ")
-plt.grid(True)
-plt.savefig('grafica.png')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('x**3 + 5')
+plt.show()
