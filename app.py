4bbc2ff46a864dd4d741b5e27502cf29298c1f2e
MeliLiz <melifernandez@ciencias.unam.mx>
2024-02-17 19:22:17 -0600

    Conexion usuarios

--- app.py
+++ app.py
@@ -2,17 +2,15 @@ from flask import Flask
 from sqlalchemy import and_, or_
 
 from alchemyClasses import db
-from alchemyClasses.Alumno import Alumno
-from cryptoUtils.CryptoUtils import cipher
-from hashlib import sha256
+from alchemyClasses.usuarios import usuarios
 
-from model.model_alumno import borra_alumno
+#from model.model_alumno import borra_alumno
 
 #mysql+pymysql://ferfong:Developer123!@localhost:3306/ing_soft
 #<dialecto>+<driver>://<usuario>:<passwd>@localhost:3306/<db>
 #mysql+pymysql://lab:Developer123!@localhost:3306/lab_ing_soft
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://ferfong:Developer123!@localhost:3306/ing_soft'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://lab:Developer123!@localhost:3306/lab_ing_software'
 app.config.from_mapping(
     SECRET_KEY='dev'
 )
@@ -20,6 +18,10 @@ db.init_app(app)
 
 if __name__ == '__main__':
     with app.app_context():
+        
+        for user in usuarios.query.all():
+            print(user)
+        
         """for alumno in Alumno.query.all(): # Select * from alumno
             print(alumno)"""
 
@@ -40,5 +42,5 @@ if __name__ == '__main__':
         #fer.ap_mat = "Baeza"
         #db.session.commit()
         #Delete
-        borra_alumno(313320679)
-        print("Borrado con éxito!")
+        #borra_alumno(313320679)
+        #print("Borrado con éxito!")
