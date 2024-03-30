4f7e2d8b7430f66080bf0c262410ec8e0f3af026
MeliLiz <melifernandez@ciencias.unam.mx>
2024-03-30 00:35:43 -0600

    Update movie

--- my-app/src/components/Pages/Movies/CRUD/Update/MovieDetail.jsx
+++ my-app/src/components/Pages/Movies/CRUD/Update/MovieDetail.jsx
@@ -1,13 +1,32 @@
 import { useParams } from "react-router-dom"
+import { isMovieRegistered, editMovie } from "../../../../../DataFunctions"
 
 export default function MovieDetail() {
 
     const params = useParams()
+    const id = parseInt(params.movieId)
+
+    const movie = isMovieRegistered(id)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        editMovie(id, e.target.title.value, e.target.genre.value, e.target.length.value, e.target.stock.value)
+    }
 
     return (
         <div>
             <h1>Movie Detail</h1>
-            <p>Movie ID: {params.movieId}</p>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="title">Title</label>
+                <input type="text" id="title" name="title" required defaultValue={movie.nombre}/>
+                <label htmlFor="genre">Genre</label>
+                <input type="text" id="genre" name="genre" required defaultValue={movie.genero}/>
+                <label htmlFor="length">Length (minutes)</label>
+                <input type="number" id="length" name="length" required defaultValue={movie.duracion}/>
+                <label htmlFor="stock">Stock</label>
+                <input type='number' id="stock" name="stock" required defaultValue={movie.inventario}/>
+                <button type="submit">Update</button>
+            </form>
         </div>
     )
 }
